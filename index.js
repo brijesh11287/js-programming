ed8195735897e562ca2d307b6b9cfa57bc4543ea
Brijesh Gupta <brijeshngi@gmail.com>
2023-09-04 20:49:07 +0530

    modified

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello Coders");
+function deleteElemnt() {
+  let arr = ["I", "study", "JavaScript", "right", "now"];
+
+  arr.splice(2, 1);
+
+  return arr;
+}
+console.log(deleteElemnt());
